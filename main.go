e908f5a45f5eb774f78ff5584bb6f2ca6ea8dae1
ynncstslv <ynncstslv@gmail.com>
2023-09-14 12:51:54 -0300

    feat: declaring model

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "github.com/charmbracelet/bubbles/list"
+
 type status int
 
 const (
@@ -26,4 +28,21 @@ func (t Task) Title() string {
 
 func (t Task) Description() string {
 	return t.description
+}
+
+/* MAIN MODEL */
+type Model struct {
+	list list.Model
+	err error
+}
+
+// TODO: call this on tea.WindowSizeMsg
+func (m *Model) initList(width, height int) {
+	m.list = list.New([]list.Item{}, list.NewDefaultDelegate(), width, height)
+	m.list.Title = "To Do"
+	m.list.SetItems([]list.Item{
+		Task{status: todo, title: "buy milk", description: "strawberry milk"},
+		Task{status: todo, title: "eat sushi", description: "negitoro roll, miso soup, something else"},
+		Task{status: todo, title: "fold laundry", description: "or wear wrinkly t-shirts lol"},
+	})
 }
