2bb6c1fd4dd4a8be9fd9517c89c884262ebe2b13
ynncstslv <ynncstslv@gmail.com>
2023-09-14 14:53:41 -0300

    feat: moving items across lists

--- main.go
+++ main.go
@@ -33,6 +33,14 @@ type Task struct {
 	description string
 }
 
+func (t *Task) Next() {
+	if t.status == done {
+		t.status = todo
+	} else {
+		t.status++
+	}
+}
+
 // implement the list.Item interface
 func (t Task) FilterValue() string {
 	return t.title
@@ -57,6 +65,16 @@ type Model struct {
 
 func New() *Model {
 	return &Model{}
+}
+
+func (m *Model) MoveToNext() tea.Msg {
+	selectedItem := m.lists[m.focused].SelectedItem()
+	selectedTask := selectedItem.(Task)
+	m.lists[selectedTask.status].RemoveItem(m.lists[m.focused].Index())
+	selectedTask.Next()
+	m.lists[selectedTask.status].InsertItem(len(m.lists[selectedTask.status].Items())-1, list.Item(selectedTask))
+
+	return nil
 }
 
 // TODO: go to next list
@@ -125,6 +143,8 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				m.Prev()
 			case "right", "l":
 				m.Next()
+			case "enter":
+				return m, m.MoveToNext
 			}
 	}
 
